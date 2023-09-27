a18c5df3dace8ec2f799a4c84bc005a8ef246cf2
BeastBeno <dietzbenony.awoussi@logmein.com>
2023-09-27 07:34:31 -0400

    remove build file

--- backend/build/endpoints/health.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-const express_1 = __importDefault(require("express"));
-const health_1 = __importDefault(require("../service/health"));
-const app = express_1.default.Router();
-app.get("/ping", health_1.default.check);
-module.exports = app;
--- backend/build/service/health.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const check = (req, res, next) => res.status(200).json({
-    message: "pong",
-});
-exports.default = { check };
